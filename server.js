00627ab260f98b3ade2ad8469b1b1f9c6c4a4768
mahmoud-nassif <mahmoudnassifptp39@gmail.com>
2020-01-10 01:59:45 +0200

    8th commit backend

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const port=process.env.PORT||3030
 const config=require('config')
 
 
-mongoose.connect(config.get('db'),{ useNewUrlParser: true ,useUnifiedTopology: true })
+mongoose.connect(config.get('db'),{ useNewUrlParser: true ,useUnifiedTopology: true }).then(done=>{
+    console.log(done)
+}).catch(err=>{
+    console.log("mongoose err",err)
+})
 console.log(`current db ${config.get('db')}`)
 
 const server=app.listen(port,()=>{
