fbffe0bc027299b081fe0bb8f8c3e1e9d0d042d2
mahmoud-nassif <mahmoudnassifptp39@gmail.com>
2020-01-10 02:15:07 +0200

    9th commit backend

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const port=process.env.PORT||3030
 const config=require('config')
 
 
-mongoose.connect(config.get('db'),{ useNewUrlParser: true ,useUnifiedTopology: true }).then(done=>{
-    console.log(done)
-}).catch(err=>{
-    console.log("mongoose err",err)
-})
-console.log(`current db ${config.get('db')}`)
+// mongoose.connect(config.get('db'),{ useNewUrlParser: true ,useUnifiedTopology: true }).then(done=>{
+//     console.log(done)
+// }).catch(err=>{
+//     console.log("mongoose err",err)
+// })
+// console.log(`current db ${config.get('db')}`)
+mongoose.connect("mongodb+srv://mahmoud-nassif:95685124951@cluster0-rhgan.mongodb.net/test?retryWrites=true&w=majority")
 
 const server=app.listen(port,()=>{
     console.log('server is ready')
